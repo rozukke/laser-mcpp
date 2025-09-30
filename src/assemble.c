434e31a97a832240d5ccbe7d304f40554e7ab1bf
darcy <acednes@gmail.com>
2025-09-30 14:29:50 +1000

    feat: promote incorrect operand count warning to error

--- src/assemble.c
+++ src/assemble.c
@@ -238,7 +238,7 @@ uint8_t passone (uint32_t ln, uint16_t *addr, TokenBuffer *buf, arrs_t *arrs)
     if (opnum == -1) {
         
     } else if (opcount > opnum) {
-        warning (ln, "'%s' expects %d operands", tok, opnum);
+        error (ln, "'%s' expects %d operands", tok, opnum);
     } else if (opcount < opnum) {
         error (ln,	"'%s' expects %d operands", tok, opnum);
     }
